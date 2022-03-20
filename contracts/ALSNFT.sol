73e68b3855c60a82052887b3a53b01176743026a
Estelle Choi <estele.choi@gmail.com>
2022-03-20 14:19:46 +0900

    setup: packages added

--- contracts/ALSNFT.sol
+++ contracts/ALSNFT.sol
@@ -3,16 +3,26 @@ pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ALSNFT is ERC721URIStorage {
+contract ALSNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private tokenIds;
 
-    constructor() ERC721("Alice Loves Sea NFT", "ALS") {}
+    constructor() ERC721("Alice Loves Sea NFT", "ALS") {
+        _transferOwnership(_msgSender());
+    }
 
-    function mint(address _toAddr, string memory _tokenURI)
-        public
-        returns (uint256)
-    {}
+    function mint(
+        address _toAddr,
+        uint256 _tokenId,
+        string memory _tokenURI
+    ) public onlyOwner returns (uint256) {
+        // tokenIds.increment(); // token ids created in order
+        uint256 newItemId = _tokenId;
+        _mint(_toAddr, newItemId);
+        _setTokenURI(newItemId, _tokenURI);
+        return newItemId;
+    }
 }
